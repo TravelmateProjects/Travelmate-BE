f49bb30d1aa6b9821e1d8cda7334a68d63ca02db
Tannpd <144095953+Tannpd@users.noreply.github.com>
2025-05-30 17:23:35 +0700

    Finish register and forgot password

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,5 +6,8 @@ const { verifyToken, verifyRefreshToken } = require('../middlewares/authMiddlewa
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/refresh-token',  authController.refreshToken);
+router.post('/verify-account', authController.verifyAccount);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
