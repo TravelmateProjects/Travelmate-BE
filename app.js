e03dd37ddba3a88c73d80f5b29d3e17b08e54987
unknown <nhatthach2703@gmail.com>
2025-06-23 07:16:22 +0700

    Update api test to test deploy for web 2

--- app.js
+++ app.js
@@ -55,9 +55,9 @@ app.use(express.urlencoded({ extended: true, limit: '20mb' })); // Set limit for
 connectDB();
 
 // Debug middleware (only in development)
-// if (process.env.NODE_ENV === 'development') {
-//     app.use('/auth', debugMiddleware);
-// }
+if (process.env.NODE_ENV === 'development') {
+    app.use('/auth', debugMiddleware);
+}
 
 // Routes
 app.use('/', indexRouter);
