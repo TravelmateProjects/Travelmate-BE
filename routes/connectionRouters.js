9096d2fbb653659d35de52a80ac7a2317fdb14b3
Duck <vomailamvy@gmail.com>
2025-06-24 20:29:21 +0700

    add remove friends

--- routes/connectionRouters.js
+++ routes/connectionRouters.js
@@ -3,13 +3,10 @@ const router = express.Router();
 const connectionController = require('../controllers/connectionController');
 const { verifyToken } = require('../middlewares/authMiddleware');
 
-// Gửi yêu cầu kết bạn
+
 router.post('/', verifyToken, connectionController.sendFriendRequest);
-
-// Chấp nhận kết bạn
 router.post('/accept', verifyToken, connectionController.acceptFriendRequest);
-
-// Từ chối kết bạn
 router.post('/reject', verifyToken, connectionController.rejectFriendRequest);
+router.delete('/remove', verifyToken, connectionController.removeFriend);
 
 module.exports = router;
